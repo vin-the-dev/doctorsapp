35c26e5e195832107a58c9a2470a3ea61fb4bfc1
Vineeth Vijayan <vinbhai4u@gmail.com>
2015-10-30 23:06:37 +0530

    initial commit

---
+++ doctorsapp/doctorsapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  doctorsapp
+//
+//  Created by Vineeth Vijayan on 30/10/15.
+//  Copyright © 2015 Vineeth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
